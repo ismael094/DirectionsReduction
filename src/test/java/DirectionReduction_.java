366001cd9ee83f790d5ad03c9a58a0865721a7ac
ismael094 <ismaelpitufo@gmail.com>
2019-10-13 19:17:39 +0100

    First test

---
+++ src/test/java/DirectionReduction_.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(org.junit.runners.Parameterized.class)
+public class DirectionReduction_ {
+
+    private final String[] number;
+    private final String[] value;
+
+    public DirectionReduction_(String[] directions, String[] value) {
+        this.number = directions;
+        this.value = value;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases() {
+        return new Object[][]{
+                {new String[]{"NORTH"}, new String[]{"NORTH"}}
+        };
+    }
+
+    @Test
+    public void execute() {
+        assertThat(reduceDirectionOf(number)).isEqualTo(value);
+    }
+
+    private String[] reduceDirectionOf(String[] directions) {
+        return new String[]{"NORTH"};
+    }
+}
